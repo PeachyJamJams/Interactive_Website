00c196d2040bf19e839214c8a814a6393d146c02
Jet <117852510+PeachyJamJams@users.noreply.github.com>
2024-11-24 18:48:06 -0600

    w

--- timeline.js
+++ timeline.js
@@ -128,3 +128,18 @@ window.onclick = function(event) {
         }
     }
 };
+
+
+// Throttle function to limit the rate at which a function can fire
+function throttle(func, limit) {
+    let inThrottle;
+    return function() {
+        const args = arguments;
+        const context = this;
+        if (!inThrottle) {
+            func.apply(context, args);
+            inThrottle = true;
+            setTimeout(() => inThrottle = false, limit);
+        }
+    }
+}
